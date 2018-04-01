2f557368bcd1864a84e72348f1a665ae63f944c7
SUNDARA MAHALINGAM <sundaramahalingam@SUNDARAs-MacBook-Air.local>
2018-04-01 11:30:20 +0530

    RoboFriends Initial Merge

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+	{
+		id:1,
+		name:'Learn ear',
+		username: 'Bret',
+		email:'Erancere@april.biz'
+	},
+	{
+		id:2,
+		name:'ararn ear',
+		username: 'Bret',
+		email:'Sincere@april.biz'
+	},
+	{
+		id:3,
+		name:'daearn ear',
+		username: 'Bret',
+		email:'darSincere@april.biz'
+	},
+	{
+		id:4,
+		name:'n ear',
+		username: 'Bret',
+		email:'NearSincere@april.biz'
+	},
+	{
+		id:5,
+		name:'Lesdn ar',
+		username: 'Bret',
+		email:'LoradSincere@april.biz'
+	},
+	{
+		id:6,
+		name:'earnear',
+		username: 'Bret',
+		email:'reaGincere@april.biz'
+	},
+	{
+		id:7,
+		name:'rn ear',
+		username: 'Bret',
+		email:'RoounSincere@april.biz'
+	},
+	{
+		id:8,
+		name:'earn ear',
+		username: 'Bret',
+		email:'EarmarsSincere@april.biz'
+	},
+	{
+		id:9,
+		name:'earn ear',
+		username: 'Bret',
+		email:'EarmarsSincere@april.biz'
+	},
+	{
+		id:10,
+		name:'earn ear',
+		username: 'Bret',
+		email:'EarmarsSincere@april.biz'
+	},
+	{
+		id:11,
+		name:'earn ear',
+		username: 'Bret',
+		email:'EarmarsSincere@april.biz'
+	}
+];
